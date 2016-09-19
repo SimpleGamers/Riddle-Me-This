e27aec654831a10234b6f7ce2eeb63104f6ef82e
Tommygun7468 <bathurstlivewire@hotmail.com>
2016-09-19 20:44:03 +1000

    trying to get the tiles working

--- main.js
+++ main.js
@@ -4,27 +4,22 @@ var context = canvas.getContext("2d");
 var startFrameMillis = Date.now();
 var endFrameMillis = Date.now();
 
-// This function will return the time in seconds since the function 
-// was last called
-// You should only call this function once per frame
 function getDeltaTime() {
     endFrameMillis = startFrameMillis;
     startFrameMillis = Date.now();
 
-    // Find the delta time (dt) - the change in time since the last drawFrame
-    // We need to modify the delta time to something we can use.
-    // We want 1 to represent 1 second, so if the delta is in milliseconds
-    // we divide it by 1000 (or multiply by 0.001). This will make our 
-    // animations appear at the right speed, though we may need to use
-    // some large values to get objects movement and rotation correct
     var deltaTime = (startFrameMillis - endFrameMillis) * 0.001;
 
-    // validate that the delta is within range
     if (deltaTime > 1)
         deltaTime = 1;
 
     return deltaTime;
 }
+
+//ABOVE DON'T TOUCH
+
+var SCREEN_WIDTH = canvas.width;
+var SCREEN_HEIGHT = canvas.height;
 
 var fps = 0;
 var fpsCount = 0;
@@ -34,17 +29,19 @@ var LAYER_COUNT = 3;
 var LAYER_BACKGROUND = 0;
 var LAYER_WALLS = 1;
 var LAYER_OBJECTIVES = 2;
-var MAP = { tw: 30, th: 30 };
+var MAP = { tw: 16, th: 16 };
 var TILE = 8;
 var TILESET_TILE = TILE * 2;
-var TILESET_PADDING = 0;
-var TILESET_SPACING = TILESET_PADDING;
+var TILESET_PADDING = 2;
+var TILESET_SPACING = 0;
 var TILESET_COUNT_X = 19;
-var TILESET_COUNT_Y = 13;
+var TILESET_COUNT_Y = 19;
 
-// load our image for our awesome riddler game
+
+
+
 var tileset = document.createElement("img");
-tileset.src = "riddlergametileset.png";
+tileset.src = "sheet.png";
 
 function drawMap() {
     for (var layerIdx = 0; layerIdx < LAYER_COUNT; layerIdx++) {
@@ -52,11 +49,9 @@ function drawMap() {
         for (var y = 0; y < level1.layers[layerIdx].height; y++) {
             for (var x = 0; x < level1.layers[layerIdx].width; x++) {
                 if (level1.layers[layerIdx].data[idx] != 0) {
-                    // the tiles in the Tiled map are base 1 (meaning a value of 0 means no tile), so subtract one from the tileset id to get the
-                    // correct tile
                     var tileIndex = level1.layers[layerIdx].data[idx] - 1;
                     var sx = TILESET_PADDING + (tileIndex % TILESET_COUNT_X) * (TILESET_TILE + TILESET_SPACING);
-                    var sy = TILESET_PADDING + (Math.floor(tileIndex / TILESET_COUNT_X)) * (TILESET_TILE + TILESET_SPACING);
+                    var sy = TILESET_PADDING + (Math.floor(tileIndex / TILESET_COUNT_Y)) * (TILESET_TILE + TILESET_SPACING);
                     context.drawImage(tileset, sx, sy, TILESET_TILE, TILESET_TILE, x * TILE, (y - 1) * TILE, TILESET_TILE, TILESET_TILE);
                 }
                 idx++;
@@ -73,7 +68,6 @@ function run() {
     var deltaTime = getDeltaTime();
     drawMap();
 
-    // update the frame counter
     fpsTime += deltaTime;
     fpsCount++;
     if (fpsTime >= 1) {
@@ -82,11 +76,12 @@ function run() {
         fpsCount = 0;
     }
 
-    // draw the FPS
     context.fillStyle = "#f00";
     context.font = "14px Arial";
     context.fillText("FPS: " + fps, 5, 20, 100);
 }
+
+//BELOW DON'T TOUCH
 
 (function() {
     var onEachFrame;
